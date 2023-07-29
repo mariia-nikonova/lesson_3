30915f00f1eca3cbcb21c021a4eb99dbea234ff9
Mariya Nikonova <mariia-nikonova@mail.ru>
2023-07-29 12:42:47 +0300

    task-21_commit

---
+++ task-21/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите значение X1: ");
+int x1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите значение Y1: ");
+int y1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите значение Z1: ");
+int z1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите значение X2: ");
+int x2 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите значение Y2: ");
+int y2 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите значение Z2: ");
+int z2 = int.Parse(Console.ReadLine()!);
+
+
+double dist= Math.Sqrt((x1-x2)*(x1-x2) +(y1-y2)*(y1-y2) + (z1-z2)*(z1-z2));
+Console.Write("Расстояние между точками в 3D пространстве = ");
+Console.Write("{0:0.00}", dist);
