403310cd1f2d8d0b52d7067c798ebee513b89445
Mariya Nikonova <mariia-nikonova@mail.ru>
2023-07-29 12:55:25 +0300

    task-23_commit

---
+++ task-23/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+int tmp = 0;
+
+for (int i = 1; i <= n; i++)
+{
+    tmp = (i * i * i);
+    Console.Write($"{tmp}, ");
+}
